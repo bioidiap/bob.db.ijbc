4ef9f8f4ffe3a35874043636459a4e1d88341a08
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2018-04-02 17:32:18 +0200

    Updated driver [skip ci]

--- bob/db/ijbc/driver.py
+++ bob/db/ijbc/driver.py
@@ -12,122 +12,120 @@ from bob.db.base.driver import Interface as BaseInterface
 
 
 def checkfiles(args):
-  """Checks existence of files based on your criteria"""
+    """Checks existence of files based on your criteria"""
 
-  from .query import Database
+    from .query import Database
 
-  protocols        = ['search_split%d' % s for s in range(1,11)]
-  protocols.extend(['compare_split%d' % s for s in range(1,11)])
+    protocols = ['search_split%d' % s for s in range(1, 11)]
+    protocols.extend(['compare_split%d' % s for s in range(1, 11)])
 
-  for p in protocols:
+    for p in protocols:
 
-    db = Database()
-    r = db.objects(protocol=p)
-    good = {}
-    bad = {}
-    # go through all files, check if they are available on the filesystem
-    for f in r:
+        db = Database()
+        r = db.objects(protocol=p)
+        good = {}
+        bad = {}
+        # go through all files, check if they are available on the filesystem
+        for f in r:
 
-      found = False
-      for e in args.extension:
+            found = False
+            for e in args.extension:
 
-        if os.path.exists(f.make_path(args.directory,e)):
-          good[f.id] = f.make_path(args.directory,e)
-          found = True
-          break
-      if not found:
-        bad[f.id] = f.make_path(args.directory,args.extension[0])
+                if os.path.exists(f.make_path(args.directory, e)):
+                    good[f.id] = f.make_path(args.directory, e)
+                    found = True
+                    break
+            if not found:
+                bad[f.id] = f.make_path(args.directory, args.extension[0])
 
-  # report
-  output = sys.stdout
-  if args.selftest:
-    from bob.db.base.utils import null
-    output = null()
+    # report
+    output = sys.stdout
+    if args.selftest:
+        from bob.db.base.utils import null
+        output = null()
 
-  if bad:
-    for id, f in bad.items():
-      output.write('Cannot find file "%s"\n' % f)
-    output.write('%d files (out of %d) were not found at "%s"\n' % \
-        (len(bad), len(r), args.directory))
-  else:
-    output.write('All files were found !!!')
+    if bad:
+        for id, f in bad.items():
+            output.write('Cannot find file "%s"\n' % f)
+        output.write('%d files (out of %d) were not found at "%s"\n' % \
+                     (len(bad), len(r), args.directory))
+    else:
+        output.write('All files were found !!!')
 
-  return 0
+    return 0
 
 
 def path(args):
-  """Returns a list of fully formed paths or stems given some file id"""
-
-  from .query import Database
-  db = Database()
-
-  output = sys.stdout
-  if args.selftest:
-    from bob.db.base.utils import null
-    output = null()
-
-  r = db.paths(args.id, prefix=args.directory, suffix=args.extension)
-  for path in r: output.write('%s\n' % path)
-
-  if not r: return 1
-
-  return 0
-
-
-class Interface(BaseInterface):
-
-
-  def name(self):
-    return 'ijbc'
-
-
-  def version(self):
-    return pkg_resources.require('bob.db.%s' % self.name())[0].version
-
-
-  def files(self):
-    basedir = pkg_resources.resource_filename(__name__, 'protocol')
-    # these are the files that are currently used in the protocol;
-    # more files might be added later, e.g., for face detection or clustering
-    filenames = [
-      "ijbc_metadata.csv",
-      "ijbc_1N_gallery_S1.csv",
-      "ijbc_1N_gallery_S2.csv",
-      "ijbc_1N_probe_img.csv",
-      "ijbc_1N_probe_mixed.csv",
-      "ijbc_1N_probe_video.csv",
-      "ijbc_11_S1_S2_matches.csv",
-      "ijbc_11_covariate_probe_reference_metadata.csv",
-      "ijbc_11_covariate_matches.csv",
-    ]
-    return [os.path.join(basedir, f) for f in filenames]
-
-
-  def type(self):
-    return 'text'
-
-
-  def add_commands(self, parser):
-
-    from . import __doc__ as docs
-    import argparse
-
-    subparsers = self.setup_parser(parser, "IJB-C database", docs)
+    """Returns a list of fully formed paths or stems given some file id"""
 
     from .query import Database
     db = Database()
 
-    # the "checkfiles" action
-    parser = subparsers.add_parser('checkfiles', help=checkfiles.__doc__)
-    parser.add_argument('-d', '--directory', help="if given, this path will be prepended to every entry returned.")
-    parser.add_argument('-e', '--extension', nargs="+", help="if given, this extension will be appended to every entry returned.")
-    parser.add_argument('--self-test', dest="selftest", action='store_true', help=argparse.SUPPRESS)
-    parser.set_defaults(func=checkfiles) #action
+    output = sys.stdout
+    if args.selftest:
+        from bob.db.base.utils import null
+        output = null()
 
-    # adds the "path" command
-    parser = subparsers.add_parser('path', help=path.__doc__)
-    parser.add_argument('-d', '--directory', help="if given, this path will be prepended to every entry returned.")
-    parser.add_argument('-e', '--extension', help="if given, this extension will be appended to every entry returned.")
-    parser.add_argument('id', type=int, nargs='+', help="one or more file ids to look up. If you provide more than one, files which cannot be found will be omitted from the output. If you provide a single id to lookup, an error message will be printed if the id does not exist in the database. The exit status will be non-zero in such case.")
-    parser.add_argument('--self-test', dest="selftest", action='store_true', help=argparse.SUPPRESS)
-    parser.set_defaults(func=path) #action
+    r = db.paths(args.id, prefix=args.directory, suffix=args.extension)
+    for path in r: output.write('%s\n' % path)
+
+    if not r: return 1
+
+    return 0
+
+
+class Interface(BaseInterface):
+    def name(self):
+        return 'ijbc'
+
+    def version(self):
+        return pkg_resources.require('bob.db.%s' % self.name())[0].version
+
+    def files(self):
+        basedir = pkg_resources.resource_filename(__name__, 'protocol')
+        # these are the files that are currently used in the protocol;
+        # more files might be added later, e.g., for face detection or clustering
+        filenames = [
+            "ijbc_11_G1_G2_matches.csv",
+            "ijbc_11_covariate_matches.csv",
+            "ijbc_11_covariate_probe_reference.csv",
+            "ijbc_1N_gallery_G1.csv",
+            "ijbc_1N_gallery_G2.csv",
+            "ijbc_1N_probe_mixed.csv",
+            "ijbc_face_detection.csv",
+            "ijbc_face_detection_ground_truth.csv",
+            "ijbc_metadata.csv",
+            "ijbc_wild_test10.csv",
+            "ijbc_wild_test11.csv",
+            "ijbc_wild_test9.csv"]
+        return [os.path.join(basedir, f) for f in filenames]
+
+    def type(self):
+        return 'text'
+
+    def add_commands(self, parser):
+        from . import __doc__ as docs
+        import argparse
+
+        subparsers = self.setup_parser(parser, "IJB-C database", docs)
+
+        from .query import Database
+        db = Database()
+
+        # the "checkfiles" action
+        parser = subparsers.add_parser('checkfiles', help=checkfiles.__doc__)
+        parser.add_argument('-d', '--directory', help="if given, this path will be prepended to every entry returned.")
+        parser.add_argument('-e', '--extension', nargs="+",
+                            help="if given, this extension will be appended to every entry returned.")
+        parser.add_argument('--self-test', dest="selftest", action='store_true', help=argparse.SUPPRESS)
+        parser.set_defaults(func=checkfiles)  # action
+
+        # adds the "path" command
+        parser = subparsers.add_parser('path', help=path.__doc__)
+        parser.add_argument('-d', '--directory', help="if given, this path will be prepended to every entry returned.")
+        parser.add_argument('-e', '--extension',
+                            help="if given, this extension will be appended to every entry returned.")
+        parser.add_argument('id', type=int, nargs='+',
+                            help="one or more file ids to look up. If you provide more than one, files which cannot be found will be omitted from the output. If you provide a single id to lookup, an error message will be printed if the id does not exist in the database. The exit status will be non-zero in such case.")
+        parser.add_argument('--self-test', dest="selftest", action='store_true', help=argparse.SUPPRESS)
+        parser.set_defaults(func=path)  # action
